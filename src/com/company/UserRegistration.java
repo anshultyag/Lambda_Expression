b611a382b70379d8fb57dac95fecbdd5e7eb15fb
anshultyag <you@example.com>
2022-02-01 01:23:41 +0530

    Added: Valid email

--- src/com/company/UserRegistration.java
+++ src/com/company/UserRegistration.java
@@ -11,11 +11,16 @@ public class UserRegistration {
     public static Validate lastName() {
         return employeeName -> employeeName.matches("^[A-Z][A-Za-z]{2,}$");
     }
+    public static Validate email() {
+        return employeeName -> employeeName.matches("^abc[.a-z]+@bl[.]co[.a-z]+");
+    }
 
    public static void main(String[] args) {
             boolean isValidName = UserRegistration.firstName().validateDetails("Anshul");
                  System.out.println(isValidName);
             boolean isValidLastName = UserRegistration.lastName().validateDetails("Tyagi");
                  System.out.println(isValidLastName);
+            boolean isValidEmailName = UserRegistration.email().validateDetails("abc.abc@bl.co.in");
+                 System.out.println(isValidEmailName);
                 }
 }
