16daead5086d0def5f5fe961769e5d258836d546
anshultyag <you@example.com>
2022-02-01 01:17:34 +0530

    Added: valid Last name

--- src/com/company/UserRegistration.java
+++ src/com/company/UserRegistration.java
@@ -8,9 +8,14 @@ public class UserRegistration {
     public static Validate firstName() {
             return employeeName -> employeeName.matches("^[A-Z][A-Za-z]{2,}$");
         }
+    public static Validate lastName() {
+        return employeeName -> employeeName.matches("^[A-Z][A-Za-z]{2,}$");
+    }
 
    public static void main(String[] args) {
             boolean isValidName = UserRegistration.firstName().validateDetails("Anshul");
                  System.out.println(isValidName);
+            boolean isValidLastName = UserRegistration.lastName().validateDetails("Tyagi");
+                 System.out.println(isValidLastName);
                 }
 }
